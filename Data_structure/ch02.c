932b438ab816a20fa7f844e98034dc814024dcaf
sumin <sumint730@naver.com>
2024-03-12 11:31:46 +0900

    초기 커밋.

---
+++ Data_structure/ch02.c
@@ -0,0 +1,8 @@
+//#include<stdio.h> 
+//int main(void) {
+//
+//	int val = 21;
+//	int val_minus = -21;
+//
+//	return 0;
+//}
---
+++ Data_structure/ch02_04.c
@@ -0,0 +1,9 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	char string[3][10] = { "Dream", "come", "true!" };
+//	char *ptr[3] = { "Dream", "come", "true!" };
+//
+//	return 0;
+//}
