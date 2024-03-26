b707916321020664eedff92a079f9bbe2b280760
sumin <sumint730@naver.com>
2024-03-26 13:39:33 +0900

    commit

--- Data_structure/hw/ch02_hw02.c
+++ Data_structure/hw/ch02_hw02.c
@@ -1,10 +1,10 @@
-//#include<stdio.h>
-//
-//int main(void) {
-//
-//	char str[3];
-//	printf("문자열을 입력하세요: %\n", );
-//	scanf_s("%s", str);
-//
-//	return 0;
-//}
+#include<stdio.h>
+
+int main(void) {
+
+	char str[3];
+	printf("문자열을 입력하세요: %\n", );
+	scanf_s("%s", str);
+
+	return 0;
+}
