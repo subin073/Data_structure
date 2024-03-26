54c452a9ada2779e03bdf1ebbab220aaef4c8ad9
sumin <sumint730@naver.com>
2024-03-26 17:03:51 +0900

    commit

--- Data_structure/hw/ch02_hw01.c
+++ Data_structure/hw/ch02_hw01.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-
-int main(void) {
-
-	char* ptr1[4] = {
-		{"Korea\n"},{"Seoul\n"},{"Mapo\n"},{"152번지2/3\n"}
-	};
-
-	char* ptr2[4] = {
-		{"Korea\n"},{"Seoul\n"},{"Jongno\n"},{"152번지2/3\n"}
-	};
-
-	for (int i = 0; i < 4; i++)
-	{
-		printf("%s", ptr1[i]);
-	}
-
-	printf("\n \n \n");
-
-	for (int i = 0; i < 4; i++)
-	{
-		printf("%s", ptr2[i]);
-	}
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	char* ptr1[4] = {
+//		{"Korea\n"},{"Seoul\n"},{"Mapo\n"},{"152번지2/3\n"}
+//	};
+//
+//	char* ptr2[4] = {
+//		{"Korea\n"},{"Seoul\n"},{"Jongno\n"},{"152번지2/3\n"}
+//	};
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		printf("%s", ptr1[i]);
+//	}
+//
+//	printf("\n \n \n");
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		printf("%s", ptr2[i]);
+//	}
+//
+//	return 0;
+//}
--- Data_structure/hw/ch02_hw02.c
+++ Data_structure/hw/ch02_hw02.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
-#include<string.h>
-
-int main(void) {
-	
-	int len = 0;
-	char str[10];
-	printf("문자열을 입력하세요:");
-	scanf_s("%s", str, sizeof(str));
-	len = strlen(str);
-
-	printf("\n\n입력된 문자열은\n");
-	printf("\"%s\"\n", str);
-	printf("입니다.\n\n");
-	printf("입력된 문자열의 길이 = %d \n", len);
-
-
-	return 0;
-}
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main(void) {
+//	
+//	int len = 0;
+//	char str[10];
+//	printf("문자열을 입력하세요:");
+//	scanf_s("%s", str, sizeof(str));
+//	len = strlen(str);
+//
+//	printf("\n\n입력된 문자열은\n");
+//	printf("\"%s\"\n", str);
+//	printf("입니다.\n\n");
+//	printf("입력된 문자열의 길이 = %d \n", len);
+//
+//
+//	return 0;
+//}
