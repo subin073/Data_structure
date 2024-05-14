a05dbab976f11ebbcc9e40b760b5a47d228ea2ba
sumin <sumint730@naver.com>
2024-05-14 10:48:45 +0900

    commit

---
+++ Data_structure/Tree/Tree/traversalBT.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "traversalBT.h"
+
+
+treeNode* makeRootNode(element data, treeNode* leftNode, treeNode* rightNode) {
+	treeNode* root = (treeNode*)malloc(sizeof(treeNode));
+	root->data = data;
+	root->left = leftNode;
+	root->right = rightNode;
+	return root;
+}
+
+
+void preorder(treeNode* root) {
+	if (root){
+		printf("%c", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+
+void inorder(treeNode* root) {
+	if (root) {
+		inorder(root->left);
+		printf("%c", root->data);
+		inorder(root->right);
+	}
+}
+
+
+void postorder(treeNode* root) {
+	if (root) {
+		postorder(root->left);
+		postorder(root->right);
+		printf("%c", root->data);
+	}
+}
