4b7aa4f97e25adc8182082ff02382c2aaf02d1ca
sumin <sumint730@naver.com>
2024-06-04 10:04:31 +0900

    commit

--- Data_structure/Sort/sort01/selectSort.c
+++ Data_structure/Sort/sort01/selectSort.c
@@ -2,7 +2,7 @@
 void SelectionSort(int a[], int size) {
 	int i, j, t, min, temp;
 
- 	for (i = 0; i < size - 1; i++) {
+	for (i = 0; i < size - 1; i++) {
 		min = i;
 		for (j = i + 1; j < size; j++) {
 			if (a[j] < a[min])	min = j;
@@ -12,5 +12,5 @@ void SelectionSort(int a[], int size) {
 		a[min] = temp;
 		printf("\n%d´Ü°è : ", i + 1);
 		for (t = 0; t < size; t++)	printf("%3d", a[t]);
- 	}
+	}
 }
